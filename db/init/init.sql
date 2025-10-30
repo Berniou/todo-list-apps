7eb2c8d0ab0daef99997cd6dbc318901a8d3b3b6
Bernardo MBANGYA NDOUMBE <bernardo.mbangya@gmail.com>
2025-10-30 11:08:14 +0100

    feat: adding database initialization request for docker container.

---
+++ db/init/init.sql
@@ -0,0 +1,82 @@
+ CREATE TABLE IF NOT EXISTS todoitems (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(1000),
+    description VARCHAR(1000),
+    dateOfCreation DATETIME DEFAULT CURRENT_DATE,
+    status INTEGER,
+    UNIQUE(title)
+    );
+
+INSERT INTO
+    todoitems
+VALUES
+    (
+        DEFAULT,
+        '"commodi commodi delectus"',
+        '"Est alias qui consequatur occaecati aspernatur."',
+        '"2065-12-05T22:25:58.326Z"',
+        '10'
+    ),
+    (
+        DEFAULT,
+        '"Ullam aspernatur sunt commodi quisquam fuga. Beatae deserunt nisi in deleniti nesciunt tenetur. Quia asperiores et et sint voluptatem. Dolore et eum temporibus et aut rerum et ex totam. Occaecati asperiores doloremque ex eius quo voluptatibus aperiam quis quia."',
+        '"Voluptatem officiis voluptatem soluta. Vel tempore rerum necessitatibus nisi. Soluta similique aliquid qui pariatur possimus aut blanditiis dolor. Eum laborum ea officiis ipsa neque expedita.\n \rSint mollitia molestiae enim doloribus qui iste hic. Qui consequatur atque perspiciatis quod hic. Delectus sint libero ad.\n \rDeleniti ex aperiam mollitia. Ab quia consequatur molestias aliquid aut fugit eum. Rerum ut earum consectetur provident aut excepturi eos. Dicta sit ut rerum id accusamus quisquam doloribus culpa."',
+        '"1996-06-04T14:32:37.992Z"',
+        '100'
+    ),
+    (
+        DEFAULT,
+        '"Nam sint ea corrupti facilis omnis qui.\nConsequatur ut consequatur aperiam nihil sint velit nihil modi.\nNon est natus.\nOdit quo minima."',
+        '"Aperiam excepturi aliquam quia repellat facere. Dicta perferendis quia ex aliquam sint. Incidunt ut nulla quo voluptas nihil vel ea. Nobis reiciendis velit accusamus eius.\n \rLaudantium molestiae voluptate odit iure ut. Dolore et ut. A sed enim explicabo. Odio laborum hic non.\n \rEa ea non aliquid sit. Sapiente dolorem eos illum expedita quia ut dicta cumque maiores. Nemo ut et. Reprehenderit veniam nobis hic distinctio eos delectus officia omnis. Ab quod voluptatem illo ab. Enim optio consectetur."',
+        '"2092-04-21T10:02:42.741Z"',
+        '1'
+    ),
+    (
+        DEFAULT,
+        '"Dolor qui quisquam expedita ad voluptas vitae nemo voluptas excepturi."',
+        '"Et cupiditate in sunt ut hic qui consequatur neque sit. Delectus illum sunt magni. Error voluptas fugit provident error aliquid recusandae nihil ipsa. Ab natus nemo officia tempora consequatur recusandae nulla quas qui.\n \rAut quis debitis totam eum. Accusamus facilis praesentium repellendus aperiam perferendis maiores esse est. Ut et vero alias debitis sunt accusamus sunt qui repellat.\n \rCorporis assumenda enim facere fugit quia distinctio et praesentium voluptas. Quia voluptatibus libero debitis aspernatur. At nulla dicta quo nam rerum dolore voluptatem. Vel id sit quisquam et quidem."',
+        '"2004-07-23T18:11:48.350Z"',
+        '1'
+    ),
+(
+        DEFAULT,
+        '"Et blanditiis cupiditate deserunt deserunt debitis excepturi nemo consequuntur. Hic et at alias aut laudantium recusandae vel excepturi aut. Molestiae necessitatibus nulla veniam. Magnam deserunt earum beatae. Ea aliquid mollitia nihil voluptates sint cum officiis. Aut autem omnis tenetur blanditiis distinctio dolorum autem qui fuga."',
+        '"Delectus sint et quia. Dolores fugiat inventore non illum qui nulla voluptates quia ad. Natus eius rerum iusto quas. Voluptas maiores est sapiente non enim libero.\n \rDolor in autem dolor. Temporibus ut repudiandae sit. Officiis esse repellat quia cupiditate aut commodi quaerat temporibus est. Quia provident praesentium minima voluptas rerum dolor. Occaecati quis excepturi velit neque. Quas suscipit eos eos beatae sunt voluptates occaecati ipsa nobis.\n \rPraesentium possimus et doloremque consequatur accusamus deserunt rem. Aut rem provident nemo. Aut assumenda sed est cupiditate recusandae nemo fuga quae corrupti. In perspiciatis aut aut non sapiente incidunt temporibus et."',
+        '"2066-01-04T22:44:50.391Z"',
+        '100'
+    ),
+(
+        DEFAULT,
+        '"Est corrupti quia numquam minus. Quaerat commodi nam occaecati sint qui ea quam. Aliquam quaerat dolorum nostrum. Labore nulla ea fugiat voluptatem laboriosam. Rem voluptatem non vel repudiandae dolorum reprehenderit expedita.\n \rSoluta odio pariatur quasi et omnis ex dolores veritatis. Natus molestiae tempore dolore sed. Fuga est ratione magnam optio molestiae.\n \rSaepe ut necessitatibus voluptas quibusdam doloremque quam omnis reiciendis voluptatem. Harum dolorem et at dolores architecto. Qui sed quaerat."',
+        '"Velit fuga sit id. Aliquam necessitatibus in tempora similique non est. Doloremque dolorum dignissimos odio unde nobis quisquam. Amet officia harum distinctio ratione magni. Eum minus laboriosam animi voluptate vitae. Soluta non assumenda."',
+        '"2097-01-16T21:56:51.746Z"',
+        '10'
+    ),
+(
+        DEFAULT,
+        '"Vel harum nobis velit beatae."',
+        '"qui dolorem ut"',
+        '"2096-08-30T00:24:14.742Z"',
+        '100'
+    ),
+(
+        DEFAULT,
+        '"Sed omnis ipsum sunt. Ut quidem tempora veniam numquam. Amet non magni. Tempore aut quidem voluptas incidunt sit dolores praesentium quos. Minima ratione laboriosam quam totam dolorum dolores laudantium aliquid. Provident autem sequi quis sed veniam qui facilis commodi non.\n \rNumquam aut et dolorum harum aliquam non. Quos quidem nisi ullam quis quas harum possimus. Odit quia officiis voluptatem. Eius iusto quia fuga dolores magni omnis. Sed magnam ab sed autem necessitatibus.\n \rQui facere possimus consequatur non et qui esse molestiae. Commodi rerum laboriosam. Assumenda delectus unde similique et voluptate nesciunt amet tempora."',
+        '"Fugit distinctio consequuntur aut qui ut cum qui eaque. Fugit a qui odit quia voluptas non earum. Veniam voluptas aut dignissimos aut quae commodi. Qui sit veniam voluptatem porro. Ipsum odio reiciendis facilis consequatur. Explicabo veritatis perspiciatis ea asperiores non voluptate non distinctio.\n \rAnimi laboriosam eos quos est ut amet eum. Corporis sint non saepe est voluptatem. Neque dolores odit placeat ipsam. Illo incidunt impedit quod et fuga. Sunt qui et aut.\n \rPerspiciatis nostrum quaerat quam beatae porro iste voluptatum accusantium at. Dolorem fugit doloremque autem possimus et ullam assumenda dolores ratione. In sit excepturi. Voluptatem numquam dolor non in repellat."',
+        '"2062-03-22T07:13:12.439Z"',
+        '1'
+    ),
+(
+        DEFAULT,
+        '"Ea tenetur dolore quis reiciendis doloribus officiis ea tenetur excepturi. Temporibus eum recusandae deserunt facere. Pariatur aut sit aspernatur sunt temporibus aut.\n \rUt nihil corrupti quos in nostrum itaque at fuga. Aut dignissimos esse id itaque. Asperiores neque inventore saepe qui.\n \rDolor quia totam aspernatur. Delectus in deleniti. Ut natus et aut omnis tempora suscipit rerum quis. Nihil accusamus nihil qui voluptatem quis praesentium dolore tempore aut. Nihil nam dolores nihil. Inventore voluptatibus et dolor fugiat."',
+        '"Numquam quaerat quis.\nQuidem quasi quidem voluptatem sit libero suscipit qui voluptatem impedit.\nFuga accusamus cumque incidunt deserunt distinctio eius.\nNihil quaerat iste quisquam.\nUt quia laudantium perspiciatis et et fugit omnis ipsa."',
+        '"2058-04-14T03:39:58.483Z"',
+        '10'
+    ),
+(
+        DEFAULT,
+        '"magni"',
+        '"Eius ex at ut sequi qui eos.\nIpsum saepe consectetur.\nConsequatur ipsa est quas repudiandae eaque quis laboriosam.\nAut temporibus est."',
+        '"2065-04-01T22:57:18.238Z"',
+        '100'
+    );
